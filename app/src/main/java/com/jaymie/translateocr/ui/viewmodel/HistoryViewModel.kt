e94098f7fe1b5af234c2675648190e68b123e6da
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2024-12-07 04:40:42 -0500

    add translation history

--- app/src/main/java/com/jaymie/translateocr/ui/viewmodel/HistoryViewModel.kt
+++ app/src/main/java/com/jaymie/translateocr/ui/viewmodel/HistoryViewModel.kt
@@ -3,11 +3,25 @@ package com.jaymie.translateocr.ui.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.jaymie.translateocr.data.model.Translation
+import com.jaymie.translateocr.data.repository.TranslationHistoryRepository
+import kotlinx.coroutines.launch
 
 class HistoryViewModel : ViewModel() {
+    private val translationHistoryRepository = TranslationHistoryRepository()
+    
     private val _translations = MutableLiveData<List<Translation>>()
     val translations: LiveData<List<Translation>> = _translations
 
-    // TODO: Load translations from database
+    fun loadTranslations() {
+        viewModelScope.launch {
+            try {
+                val history = translationHistoryRepository.getTranslations()
+                _translations.value = history
+            } catch (e: Exception) {
+                _translations.value = emptyList()
+            }
+        }
+    }
 }
