ca389dd0a8b931a616b823394303a0e960633d8a
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2024-12-07 12:21:41 -0500

    feat: offline-translation

--- app/src/main/java/com/jaymie/translateocr/data/model/Language.kt
+++ app/src/main/java/com/jaymie/translateocr/data/model/Language.kt
@@ -2,6 +2,8 @@ package com.jaymie.translateocr.data.model
 
 data class Language(
     val code: String,
-    val name: String
+    val name: String,
+    val isDownloaded: Boolean = false,
+    val isDownloading: Boolean = false
 ) {
 }
