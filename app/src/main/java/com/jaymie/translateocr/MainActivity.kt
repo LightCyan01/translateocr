5c1df55dda8b444f39193ae7c34ca14cd5139b30
Jaymie <29302715+LightCyan01@users.noreply.github.com>
2024-12-07 12:20:33 -0500

    add notifications

--- app/src/main/java/com/jaymie/translateocr/MainActivity.kt
+++ app/src/main/java/com/jaymie/translateocr/MainActivity.kt
@@ -11,13 +11,20 @@ import com.jaymie.translateocr.databinding.ActivityMainBinding
 import com.jaymie.translateocr.ui.view.Login
 import com.jaymie.translateocr.ui.view.Profile
 import com.jaymie.translateocr.utils.ProfileImageManager
+import android.content.Context
+import android.os.Build
+import android.Manifest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.ActivityResultLauncher
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val auth = FirebaseAuth.getInstance()
+    private val prefs by lazy { getPreferences(Context.MODE_PRIVATE) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        checkNotificationPermission()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -50,5 +57,14 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+    }
+
+    private fun checkNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            !prefs.getBoolean("notification_permission_checked", false)) {
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                prefs.edit().putBoolean("notification_permission_checked", true).apply()
+            }.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
     }
 }
